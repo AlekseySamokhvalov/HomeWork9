d589e133bef0645ef4097c6aea2d50ae4bc9118c
AlekseySamokhvalov <6560@mail.ru>
2023-06-23 12:42:53 +0300

    Добавил генерацию массива отдельным методом.

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,15 @@ public class Main {
             task3();
             task4();
         }
+         // Объявить метод «сгенерироватьМассив»
+        public static int[] generateRandomArray() {
+            java.util.Random random = new java.util.Random();
+            int[] arr = new int[30];
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = random.nextInt(100_000) + 100_000;
+            }
+            return arr;
+        }
 
         public static void task1 () {
             System.out.println("Задача 1");
